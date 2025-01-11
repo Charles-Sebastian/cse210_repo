d86c8c22e5c8303ec0f27b8574f5f6f485e8c7d1
Charles-Sebastian <charles06@byui.edu>
2025-01-11 04:07:10 -0700

    Did the assignments due on January 11th

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int a = 90;
+        int b = 80;
+        int c = 70;
+        int d = 60;
+
+        string gradeLetter;
+
+        Console.Write("What is your grade percentage? ");
+        string gradePercentageString = Console.ReadLine();
+        int gradePercentageInt = int.Parse(gradePercentageString);
+
+        if (gradePercentageInt >= a)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradePercentageInt >= b)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradePercentageInt >= c)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradePercentageInt >= d)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        if (gradeLetter == "A" || gradeLetter == "F")
+        {
+            Console.WriteLine($"Your grade is an {gradeLetter}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is a {gradeLetter}");
+        }
+
+        if (gradePercentageInt < c)
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+        else
+        {
+            Console.WriteLine("Great Job!!!");
+        }
     }
 }
